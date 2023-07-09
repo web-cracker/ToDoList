1bd5fd91e48e7474354a5af0304b07fb7979e5e7
Zuber Ahamad <mdzuberahamad825@gmail.com>
2023-07-09 18:03:27 +0530

    Add files via upload
    updated the task 1

--- script.js
+++ script.js
@@ -4,23 +4,23 @@ const taskList = document.getElementById('taskList');
 
 taskForm.addEventListener('submit', function(e) {
   e.preventDefault();
-  
+
   const taskText = taskInput.value.trim();
-  
+
   if (taskText !== '') {
     const taskItem = document.createElement('li');
     taskItem.className = 'task-item';
-    
+  
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
-    
+  
     const taskTextSpan = document.createElement('span');
     taskTextSpan.textContent = taskText;
-    
+  
     taskItem.appendChild(checkbox);
     taskItem.appendChild(taskTextSpan);
     taskList.appendChild(taskItem);
-    
+  
     taskInput.value = '';
   }
 });
